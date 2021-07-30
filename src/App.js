b3331fc86cba16d16b35c1c18da670efe7f27744
leandro matias suarez <leandro_colocdk@hotmail.com>
2021-07-30 20:20:20 -0300

    Iterar elemento con map, Sacar button update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
+import { useState } from 'react';
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/newExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Internet",
+    amount: 91,
+    date: new Date(2021, 5, 15),
+  },
+  {
+    id: "e2",
+    title: "House Insurance",
+    amount: 500,
+    date: new Date(2021, 5, 20),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 295,
+    date: new Date(2021, 5, 28),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Internet",
-      amount: 91,
-      date: new Date(2021, 5, 15),
-    },
-    {
-      id: "e2",
-      title: "House Insurance",
-      amount: 500,
-      date: new Date(2021, 5, 20),
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 295,
-      date: new Date(2021, 5, 28),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
 
   const addExpenseHandler = (expense) => {
-    console.log('In App.js')
-    console.log(expense)
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses]
+    })
   }
 
   return (

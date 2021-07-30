0ec35a7005331d80faabd13a9e3942cdb77af099
leandro matias suarez <leandro_colocdk@hotmail.com>
2021-07-30 18:18:29 -0300

    Comunicacion entre componentes

--- src/App.js
+++ src/App.js
@@ -22,10 +22,16 @@ const App = () => {
       date: new Date(2021, 5, 28),
     },
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js')
+    console.log(expense)
+  }
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses
         items={expenses}
       />

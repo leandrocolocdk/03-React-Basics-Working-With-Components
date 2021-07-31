aabc88f44d48911d2a2164d51e6bb2e3d6952d26
leandro matias suarez <leandro_colocdk@hotmail.com>
2021-07-30 22:57:18 -0300

    Funcionalidad para filtrar

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,23 +5,31 @@ import "./Expenses.css"
 import Card from "../UI/Card"
 
 const Expenses = (props) => {
-    const [filteredYear, setFilterYear] = useState('2020')
+    const [filteredYear, setFilterYear] = useState('')
 
     const filterChangeHandler = (selectedYear) => [
         setFilterYear(selectedYear)
     ]
+
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear
+    })
+
+    let expensesContent = <p>No Expenses found.</p>;
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map(expense =>
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+        )
+    }
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-
-            {props.items.map(expense =>
-                <ExpenseItem
-                    key={expense.id}
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date}
-                />)}
-
+            {expensesContent}
         </Card>
     )
 
